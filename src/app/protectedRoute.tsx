ccc3ced6a687cd445479908aef5f495a910cb86e
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2024-12-02 23:10:58 +0500

    feat: выход из профиля по кнопке выйти из проифля

--- src/app/protectedRoute.tsx
+++ src/app/protectedRoute.tsx
@@ -1,12 +1,13 @@
-import React, { FC } from "react";
+import { FC, useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useLazyCheckAuthQuery } from "../store/auth-api/authApi";
 import { paths } from "./paths";
+//import { useAppSelector } from "../store/store";
 
 const ProtectedRoute: FC = () => {
   const [checkAuth, data] = useLazyCheckAuthQuery();
 
-  React.useEffect(() => {
+  useEffect(() => {
     checkAuth();
   }, []);
 
