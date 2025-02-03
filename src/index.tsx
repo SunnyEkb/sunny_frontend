b9c93c62baf08245c76c6d03bd7b81253bf6d987
Kirill Karpov <rin.veber@gmail.com>
2025-02-03 22:24:07 +0400

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
+import './normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
