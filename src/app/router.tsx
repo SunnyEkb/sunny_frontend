3cd077267bd24279ebe98e36f14cc91228e15ba0
Kirill Karpov <rin.veber@gmail.com>
2025-02-06 22:15:40 +0400

    fix

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,11 +4,15 @@ import Login from "../user/pages/logIn/logIn";
 import UserLK from "../user/pages/userLK/userLK";
 import Catalogs, { loaderCatagories } from "../ads/pages/catalogs/catalogs";
 import Catalog from "../ads/pages/catalog/catalog";
-import CardCatalogBig, { loaderAdsByCatalogId } from "../ads/pages/CardCatalogBig/CardCatalogBig";
+import CardCatalogBig, {
+  loaderAdsByCatalogId,
+} from "../ads/pages/CardCatalogBig/CardCatalogBig";
 import Registr from "../user/pages/singin/SignIn";
 import ProtectedRoute from "./protectedRoute";
 import PolicyPage from "../user/pages/PolicyPage/PolicyPage";
-import TypeCatalog, { loaderTypesCatalog } from "../ads/pages/typeCatalog/TypeCatalog";
+import TypeCatalog, {
+  loaderTypesCatalog,
+} from "../ads/pages/typeCatalog/TypeCatalog";
 import MainLayout from "./layouts/MainLayout";
 import SettingsUser from "../user/pages/settingsUser/SettingsUser";
 import UserProfileEdit from "../user/pages/UserProfileEdit/UserProfileEdit";
@@ -19,7 +23,7 @@ import MainFormAds from "../ads/pages/createAds/mainForm";
 import PasswordRecovery from "../user/pages/passwordRecovery/passwordRecovery";
 import NewPassword from "../user/pages/newPassword/newPassword";
 import RegisterActivatePage from "../user/pages/registerActivate/registerActivate";
-
+import ChatPage from "../ads/pages/chatPage/chatPage";
 
 export const router = createBrowserRouter(
   [
@@ -37,6 +41,15 @@ export const router = createBrowserRouter(
         {
           path: paths.user_profile_edit,
           element: <UserProfileEdit />,
+        },
+      ],
+    },
+    {
+      element: <MainLayout isShowFooter={false} />,
+      children: [
+        {
+          path: paths.chat,
+          element: <ChatPage />,
         },
       ],
     },
@@ -107,103 +120,8 @@ export const router = createBrowserRouter(
       element: <RegisterActivatePage />,
     },
   ],
-  {
-
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: paths.profile,
-        element: <UserLK />,
-      },
-      {
-        path: paths.settings,
-        element: <SettingsUser />,
-      },
-      {
-        path: paths.user_profile_edit,
-        element: <UserProfileEdit />,
-      },
-    ],
-  },
 
   {
-    element: <MainLayout isShowFooter={false} />,
-    children: [
-      {
-        path: paths.chat,
-        element: <ChatPage />,
-      },
-    ],
-  },
-
-  {
-    element: <MainLayout />,
-    children: [
-      {
-        path: paths.catalog,
-        element: <Catalog />,
-      },
-      {
-        path: paths.catalogAds,
-        element: <CardCatalogBig />,
-        loader: (params) => loaderAdsByCatalogId(params),
-      },
-      {
-        path: paths.createAds,
-        element: <CreateAds />,
-        children: [
-          {
-            path: paths.createAds,
-            element: <ChooseAds />,
-          },
-          {
-            path: paths.createAds + "/type",
-            element: <ChooseTypeAds />,
-          },
-          {
-            path: paths.createAds + "/type" + "/ads",
-            element: <MainFormAds />,
-          },
-        ],
-      },
-      {
-        path: paths.typeCatalog,
-        element: <TypeCatalog />,
-        loader: loaderTypesCatalog,
-      },
-      {
-        path: paths.index,
-        element: <Catalogs />,
-        loader: () => loaderCatagories(),
-      },
-    ],
-  },
-
-  {
-    path: paths.auth,
-    element: <Login />,
-  },
-  {
-    path: paths.forgetPassword,
-    element: <NewPassword />,
-  },
-  {
-    path: paths.passwordRecovery,
-    element: <PasswordRecovery />,
-  },
-  {
-    path: paths.register,
-    element: <Registr />,
-  },
-  {
-    path: paths.policy,
-    element: <PolicyPage />,
-  },
-  {
-    path: paths.registryActivate,
-    element: <RegisterActivatePage />,
-  },
-]);
     future: {
       v7_startTransition: true,
       v7_relativeSplatPath: true,
@@ -214,4 +132,3 @@ export const router = createBrowserRouter(
     },
   }
 );
-
