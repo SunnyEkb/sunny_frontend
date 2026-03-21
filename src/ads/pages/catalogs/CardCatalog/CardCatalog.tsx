bfa31022e1739acd4f7519e4b2137ddd588cb658
Teoom <ararama199915@gmail.com>
2026-03-21 21:06:35 +0700

    chore: main page categories

--- src/ads/pages/catalogs/CardCatalog/CardCatalog.tsx
+++ src/ads/pages/catalogs/CardCatalog/CardCatalog.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { paths } from "../../../../app/paths";
 import imgDefault from "../../../../assets/icon/cake.svg";
 import styles from "./card.module.scss";
@@ -13,21 +12,20 @@ interface Props {
   card: CardType;
 }
 
-export default function CardCatalog({ card }: Props) {
-  const navigate = useNavigate();
-  return (
-    <div
-      className={styles.card}
-      onClick={() => navigate(`${paths.catalogs}/${card.id}`)}
-    >
-      <h3 className={styles.card__title} >{card.title}</h3>
+const CardCatalog = ({ card }: Props) => (
+  <article className={styles.card}>
+    <a className={styles.card__link} href={`${paths.catalogs}/${card.id}`}>
       <div className={styles.card__imgWrapper}>
         <img
           src={card.img || imgDefault}
           alt="картинка каталога"
           className={styles.card__img}
         />
-      </div>
-    </div>
-  );
-}
+        <div></div>
+      </div>{" "}
+      <h3 className={styles.card__title}>{card.title}</h3>
+    </a>
+  </article>
+);
+
+export default CardCatalog;
