59e7c554acc1c9135cb34cc304037dcc8afd3e21
Aram <aram9918@yandex.ru>
2026-07-21 23:29:28 +0700

    feat: complete chat page layout

--- src/ads/pages/chatPage/WindowChat/WindowChat.tsx
+++ src/ads/pages/chatPage/WindowChat/WindowChat.tsx
@@ -3,7 +3,8 @@ import styles from "./styles.module.scss";
 import Message from "../Message/Message";
 import { useAppDispatch, useAppSelector } from "../../../../store/store";
 import { CHATWsSendMessage } from "../../../../store/actions/chat";
-import Camera from "../../../../assets/icon/camera.svg?react";
+import SendIcon from "../../../../assets/icon/send-icon.svg?react";
+import PinIcon from "../../../../assets/icon/pin-icon.svg?react";
 
 interface Props {
   adId?: string;
@@ -17,10 +18,11 @@ export default function WindowChat({
   currentUserId,
 }: Props) {
   const [value, setValue] = React.useState("");
-  const today: Date = new Date();
   const dispatch = useAppDispatch();
+
   const { currentMessages } = useAppSelector((state) => state.wsChat);
   const user = useAppSelector((state) => state.auth.user);
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
 
   function formatDate(date: Date): string {
     const options: Intl.DateTimeFormatOptions = {
@@ -31,22 +33,22 @@ export default function WindowChat({
     return date.toLocaleDateString("ru-RU", options);
   }
 
-  const formattedDate = formatDate(today);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
     setValue(e.currentTarget.value);
+    const textarea = e.target;
+    textarea.style.height = "auto";
+    textarea.style.height = `${textarea.scrollHeight}px`;
+    textarea.style.overflowY =
+      textarea.scrollHeight > 140 ? "scroll" : "hidden";
   };
 
   const handleSend = () => {
     const message = value.trim();
 
-    if (
-      !message ||
-      !adId ||
-      !recipientId ||
-      recipientId === currentUserId
-    ) {
+    // Устанавливаем новую высоту
+
+    if (!message || !adId || !recipientId || recipientId === currentUserId) {
       return;
     }
 
@@ -70,34 +72,37 @@ export default function WindowChat({
       }),
     );
     setValue("");
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.overflowY = "hidden";
+    }
   };
 
   return (
     <div className={styles.window}>
-      <div className={styles.window__dayTitle}>{formattedDate}</div>
-
       <div className={styles.window__listMessage}>
-        {currentMessages.map((item) => {
-          return <Message key={item.id} message={item} />;
-        })}
+        {!!currentMessages.length &&
+          currentMessages.map((item, index) => {
+            const formattedDate = formatDate(new Date(item.created_at));
+            const prevMess = index > 0 ? currentMessages[index - 1] : null;
+            const prevDate = prevMess
+              ? formatDate(new Date(prevMess.created_at))
+              : null;
+
+            const showDate = index === 0 || formattedDate !== prevDate;
+
+            return (
+              <>
+                {showDate && (
+                  <div className={styles.window__dayTitle}>{formattedDate}</div>
+                )}
+                <Message key={item.id} message={item} />
+              </>
+            );
+          })}
       </div>
 
       <div className={styles.window__wrapperSendInput}>
-        <input
-          placeholder="Написать сообщение"
-          className={styles.window__inputText}
-          value={value}
-          name="message"
-          onChange={(e) => handleChange(e)}
-          disabled={!adId || !recipientId || recipientId === currentUserId}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              e.preventDefault();
-              handleSend();
-            }
-          }}
-        />
-
         <label className={styles.fileLabel}>
           <input
             type="file"
@@ -110,9 +115,37 @@ export default function WindowChat({
             tabIndex={0}
             className={styles.window__iconButton}
           >
-            <Camera color="rgb(178,189,199)" />
+            <PinIcon
+              width="20"
+              height="20"
+              fill="#b2bdc7"
+              style={{ transform: "rotate(323deg)" }}
+            />
           </span>
         </label>
+        <textarea
+          ref={textareaRef}
+          placeholder="Написать сообщение"
+          className={styles.window__textAreaText}
+          value={value}
+          name="message"
+          onChange={(e) => handleChange(e)}
+          disabled={!adId || !recipientId || recipientId === currentUserId}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              handleSend();
+            }
+          }}
+        />
+
+        <button
+          className={styles.window__sendButton}
+          onClick={handleSend}
+          disabled={!adId || !recipientId || recipientId === currentUserId}
+        >
+          <SendIcon width="20" height="20" />
+        </button>
       </div>
     </div>
   );
--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -5,16 +5,21 @@ import styles from "./MainLayout.module.scss";
 import Header from "../../user/components/header/Header";
 import { paths } from "../paths";
 import { ReactNode } from "react";
+import { useMediaQuery } from "../../shared/hooks/useMediaQuery";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   const location = useLocation();
   const isModerationPage = location.pathname === paths.moderation;
+  const isChatPage = location.pathname.includes("/chat");
+  const isMobile = useMediaQuery("(max-width: 480px)");
 
   return (
     <div className={styles.container}>
       {!isModerationPage && <Header />}
-      <div className={styles.content}>{children}</div>
-      {!isModerationPage && <PcFooter />}
+      <div className={isChatPage ? styles.contentChat : styles.content}>
+        {children}
+      </div>
+      {!isModerationPage && isMobile && isChatPage ? <></> : <PcFooter />}
       {!isModerationPage && <FooterButtonMain />}
     </div>
   );
